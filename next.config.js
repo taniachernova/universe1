8fa581c69a67845ee696585f96198e4ba233426b
tania chernova <taniachernova311@gmail.com>
2024-05-20 19:08:51 +0300

    Testtest

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     return [
       {
         source: "/(.*)",
-        destination: "https://www.universeescort.com/$1",
+        destination: "https://www.universeescort.com",
         permanent: true,
         basePath: false,
       },
